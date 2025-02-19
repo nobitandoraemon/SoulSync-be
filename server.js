e4085dc79fd91c949e1dc60d2aa94901cfa65205
Lilyhouser <houselily010@gmail.com>
2025-02-19 11:19:50 +0700

    add socket

--- server.js
+++ server.js
@@ -4,18 +4,21 @@ require('dotenv').config();
 const http = require('http');
 const mongoose = require('mongoose');
 const connectDB = require('./config/connectDB');
+const socket = require('./socket/socket');
+const cookieParser = require('cookie-parser');
 const port = 3500;
 
 const server = http.createServer(app);
 connectDB();
+socket(server);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.send('Hello World! toi la nga');
+    res.send('Hello World!');
 });
-
-app.get('/login', (req, res) => {
-    res.send('this is login');
-})
 
 mongoose.connection.once('open', () => {
     console.log("Database connected...");

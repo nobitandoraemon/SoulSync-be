9fe41c4c4150b0ace98420af72788849e1d933d7
Kazelf <166615901+Kazelf@users.noreply.github.com>
2025-02-19 19:32:22 +0700

    Thai - MessageAPI

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
 const http = require('http');
 const mongoose = require('mongoose');
@@ -9,14 +10,19 @@ const port = 3500;
 const server = http.createServer(app);
 connectDB();
 
+//MIDDLEWARE
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World! :>>>');
 });
 
-app.get('/login', (req, res) => {
-    res.send('this is login');
-})
+//ROUTER
+app.use('/messages', require('./routes/api/messageRouter'));
 
+//CONNECT TO DATABASE
 mongoose.connection.once('open', () => {
     console.log("Database connected...");
     

94a6f4910d42a9cd859b22c827d45cf2ec6150aa
Lilyhouser <houselily010@gmail.com>
2025-02-26 20:29:17 +0700

    edit main

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ app.get('/', (req, res) => {
 
 app.use('/messages', require('./routes/api/messageRouter'));
 app.use('/users', require('./routes/api/userRouter'));
+app.use('/register', require('./routes/registerRouter'));
+
+app.use('/auth', require('./routes/authRouter'));
 
 mongoose.connection.once('open', () => {
     console.log("Database connected...");

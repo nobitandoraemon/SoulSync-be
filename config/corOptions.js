63dda429897a721b3af6ccdaaa5640b9b8883c28
Kazelf <166615901+Kazelf@users.noreply.github.com>
2025-03-13 14:15:09 +0700

    cors credentials: true

--- config/corOptions.js
+++ config/corOptions.js
@@ -1,8 +1,9 @@
-// const credentials = require('../middleware/credentials');
+const credentials = require('../middleware/credentials');
 const allowedOrigins = require('./allowedOrigins');
 
 const corOptions = {
     origin: allowedOrigins,
+    credentials: true,
     optionsSuccessStatus: 200
 }
 
